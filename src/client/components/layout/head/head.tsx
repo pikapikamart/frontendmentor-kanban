0c0626dc23f0276650299df90bcc1c3e28ff69ec
pikapikamart <itsmelight1@gmail.com>
2023-03-28 15:06:18 +0800

    placed the fonts link in the _document from layout/head

--- src/client/components/layout/head/head.tsx
+++ src/client/components/layout/head/head.tsx
@@ -13,10 +13,6 @@ const HTMLHead = () =>{
       <meta name="keywords" content="Kanban task, task, kanban, frontendmentor" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       {/* change title */}
-      {/* Fonts */}
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-      <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,400;0,500;0,600;0,700;1,500;1,600;1,700&display=swap" rel="stylesheet" />
       <title>FrontendMentor | Kanban Task</title>
     </Head>
   )
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -40,6 +40,9 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,400;0,500;0,600;0,700;1,500;1,600;1,700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
