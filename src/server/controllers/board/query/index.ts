c4dce43d1b5bfdc3ecf0b218aea70bcdc0e69809
pikapikamart <itsmelight1@gmail.com>
2023-06-05 13:08:44 +0800

    add population of task when getting all boards for the user

--- src/server/controllers/board/query/index.ts
+++ src/server/controllers/board/query/index.ts
@@ -1,12 +1,17 @@
 import { UserContext } from "@/server/middleware/token";
 import { boardsWithTasks } from "./schema";
 import { trpcSuccess } from "@/server/utils/trpc";
+import { taskModel } from "@/server/models/task";
 
 
 export const getAllBoardWithTasksController = async({ user }: UserContext) =>{
   
   await user.populate({
-    path: "boards"
+    path: "boards",
+    populate: {
+      path: "column.tasks",
+      model: taskModel
+    }
   })
   
   return trpcSuccess(boardsWithTasks.parse(user.boards), "Boards")
